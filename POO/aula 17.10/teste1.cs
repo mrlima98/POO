50e936356a0a7c23e0deab2ac6de39f74e4f3d9e
Michel Rocha <115085458+mrlima98@users.noreply.github.com>
2022-10-20 11:28:12 +0000

    .

--- POO/aula 17.10/teste1.cs
+++ POO/aula 17.10/teste1.cs
@@ -3,6 +3,10 @@ using System;
 public class Program {
 
   public static void Main(string[] args) {
-    Console.WriteLine("hello");
+    double x = 15.1;
+    Console.WriteLine(x);
+
+    string y = x.ToString("0.00000");
+    Console.WriteLine(y);
   }
 }
