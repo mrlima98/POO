cad082a2232cf1a0c1951fd0baa5cb4c601595b7
Michel Rocha <115085458+mrlima98@users.noreply.github.com>
2022-10-23 13:49:19 +0000

    .

---
+++ POO/Lista1/Q1.cs
@@ -0,0 +1,9 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("Michel Rocha Miranda de lima");
+    Console.WriteLine("20221014040017");
+  }
+}
