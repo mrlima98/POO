edc7399707b4040944a5fa59b0ec769d5dfbdd01
Michel Rocha <115085458+mrlima98@users.noreply.github.com>
2022-10-23 14:31:56 +0000

    .

---
+++ POO/Lista2/Q1.cs
@@ -0,0 +1,12 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+  string nome;
+    Console.WriteLine("Digite o primeiro nome: ");
+    nome = Console.ReadLine();
+    Console.WriteLine("Bem-vindo ao C#, "+nome);
+    
+  }
+}
