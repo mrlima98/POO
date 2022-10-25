09037297ac84574a7f8938a61f3ef80b12988339
Michel Rocha <115085458+mrlima98@users.noreply.github.com>
2022-10-25 12:27:46 +0000

    .

--- POO/Lista3/1018.cs
+++ POO/Lista3/1018.cs
@@ -2,7 +2,23 @@ using System;
 
 public class Program {
 public static void Main (string [] args){
+  int r, cem, cinquenta, vinte, dez, cinco, dois, um;
+  r = int.Parse(Console.ReadLine());
+  cem = r/100;
+  cinquenta = (r%100)/50;
+  vinte = ((r%100)%50)/20;
+  dez = (((r%100)%50)%20)/10;
+  cinco = ((((r%100)%50)%20)%10)/5;
+  dois = (((((r%100)%50)%20)%10)%5)/2;
+  um = ((((((r%100)%50)%20)%10)%5)%2)/1;
 
-  Console.WriteLine("oi");
+  Console.WriteLine(cem+" nota (s) de R$ 100,00");
+  Console.WriteLine(cinquenta+" nota (s) de R$ 50,00");
+  Console.WriteLine(vinte+" nota (s) de R$ 20,00");
+  Console.WriteLine(cinco+" nota (s) de R$ 5,00");
+  Console.WriteLine(dois+" nota (s) de R$ 2,00");
+  Console.WriteLine(vinte+" nota (s) de R$ 20,00");
+  Console.WriteLine(um+" nota (s) de R$ 1,00");
+    
 }
 }
---
+++ POO/Lista3/2377.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program{
+  public static void Main (string [] args){
+    int l,d,k,p;
+    string vetor [] = Console.ReadLine().Split(' ');
+    
+    
+  }
+}
