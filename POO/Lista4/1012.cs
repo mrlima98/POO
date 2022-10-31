1029daa77cef1c597bb5de2ee534c3da3fff58b2
Michel Rocha <115085458+mrlima98@users.noreply.github.com>
2022-10-31 13:30:53 +0000

    Inicio Lista4

---
+++ POO/Lista4/1012.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program{
+
+  public static void Main(string [] args){
+   string [] vetor = Console.ReadLine().Split(' ');
+   double a = double.Parse(vetor[0]);
+     double b = double.Parse(vetor[1]);
+     double c = double.Parse(vetor[2]);
+    Console.WriteLinet(b);
+  }
+
+}
