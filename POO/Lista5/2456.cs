1e0581fa1e9ee4100dc74e0b3bf8dfa3af6ddbe6
Michel Rocha <115085458+mrlima98@users.noreply.github.com>
2022-11-03 13:07:47 +0000

    Lista5

--- POO/Lista5/2456.cs
+++ POO/Lista5/2456.cs
@@ -2,6 +2,33 @@ using System;
 
 public class Program{
   public static void Main(string [] args){
-    Console.WriteLine("oi");
+    string [] aposta = Console.ReadLine().Split(' ');
+    string [] sorteado = Console.ReadLine().Split(' ');
+    int i = 0;
+    int y = 0;
+    int cont = 0;
+    for (i=0;i<6;i++){
+      for(y=0;y<6;y++){
+        if(aposta[i]==sorteado[y]){
+          cont++;
+        }
+      }
+    }
+    if (cont==3){
+      Console.WriteLine("terno");
+    }
+    else if (cont==4){
+      Console.WriteLine("quadra");
+    }
+    else if (cont==5){
+      Console.WriteLine("quina");
+    }
+    else if (cont==6){
+      Console.WriteLine("sena");
+    }
+    else{
+      Console.WriteLine("azar");
+    }
+    
   }
 }
